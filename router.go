bd3ac890d76b3a3e1f6cffda48e90d347bfa4596
Stefan Blazhevski <stefan.blazhevski@minerva.dev>
2024-07-05 18:36:13 +0200

    Moving router to separate package

---
+++ router.go
@@ -0,0 +1,60 @@
+package router
+
+import (
+	"html/template"
+	"net/http"
+)
+
+// Router
+type Router struct {
+	Routes    map[string]string
+	Directory string
+	Renderer  func(w http.ResponseWriter, templateFile string) error
+}
+
+// Instantiate new router
+func NewRouter(renderer, templatesDir string) *Router {
+	r := &Router{
+		Routes:    make(map[string]string),
+		Directory: templatesDir,
+	}
+
+	r.Renderer = r.defaultTemplateRenderer
+
+	return r
+}
+
+func (r *Router) defaultTemplateRenderer(w http.ResponseWriter, templatePath string) error {
+	tmpl, err := template.ParseFiles(templatePath)
+	if err != nil {
+		http.Error(w, "No template found", http.StatusNotFound)
+	}
+
+	err = tmpl.Execute(w, nil)
+	if err != nil {
+		http.Error(w, "Failed to open template", http.StatusInternalServerError)
+	}
+
+	return nil
+}
+
+// Add Route to Router
+func (r *Router) AddRoute(path string, template string) {
+	r.Routes[path] = template
+}
+
+func (r *Router) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	path := req.URL.Path
+
+	// Check for exact match
+	if templatePath, ok := r.Routes[path]; ok {
+		err := r.Renderer(w, templatePath+".html")
+		if err != nil {
+			http.Error(w, "Error rendering template", http.StatusInternalServerError)
+		}
+		return
+	}
+
+	// If no match found, return 404
+	http.NotFound(w, req)
+}
